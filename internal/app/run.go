10b1c3c39fe2fd127d89c719e99b1848329c490d
root <nameerrror3301@gmail.com>
2022-12-11 06:22:31 +0200

    Исправил недоразумение с множеством однотипных структур и подготовил почву под будущию сборку сервиса в контейнере

--- internal/app/run.go
+++ internal/app/run.go
@@ -10,50 +10,57 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	Cars       = "Cars"
+	Electronic = "Electronic"
+	Motos      = "Motos"
+	Home       = "Home"
+	PhotoTV    = "PhotoTV"
+)
+
 func Run() {
 	var wg sync.WaitGroup
 	var urlSlug string = "https://es.wallapop.com/item"
-	var outCars, outElectronic, outMotos, outHome, outPhotoTV []byte
 
 	for i := 40; i <= 400; {
 		wg.Add(5)
 		var (
-			urlCarCat        = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=b5492bdb-1e92-4a18-bc2d-e06fe6b04501&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=120&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=12800&longitude=-3.69196", strconv.Itoa(i))
-			urlElectronicCat = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=c32b03a2-07a3-40cc-bd3c-c2a2a05c5abb&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=80&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=15000&longitude=-3.69196", strconv.Itoa(i))
-			urlMotosCat      = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=723dc55e-d048-4332-948b-7c84f8d1f1de&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=40&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=14000&longitude=-3.69196", strconv.Itoa(i))
-			urlHomeCat       = fmt.Sprintf("https://api.wallapop.com/api/v3/real_estate/search?filters_source=seo_landing&latitude=40.41956&start=%s&order_by=most_relevance&step=0&category_ids=200&longitude=-3.69196&search_id=70de3634-8711-4d63-b4e6-4b5c7e828383", strconv.Itoa(i))
-			urlPhotoTVCat    = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=1e5fb024-9140-495d-92b5-47710871c000&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=200&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=12545&longitude=-3.69196", strconv.Itoa(i))
+			urlCar        = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=b5492bdb-1e92-4a18-bc2d-e06fe6b04501&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=120&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=12800&longitude=-3.69196", strconv.Itoa(i))
+			urlElectronic = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=c32b03a2-07a3-40cc-bd3c-c2a2a05c5abb&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=80&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=15000&longitude=-3.69196", strconv.Itoa(i))
+			urlMotos      = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=723dc55e-d048-4332-948b-7c84f8d1f1de&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=40&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=14000&longitude=-3.69196", strconv.Itoa(i))
+			urlHome       = fmt.Sprintf("https://api.wallapop.com/api/v3/real_estate/search?filters_source=seo_landing&latitude=40.41956&start=%s&order_by=most_relevance&step=0&category_ids=200&longitude=-3.69196&search_id=70de3634-8711-4d63-b4e6-4b5c7e828383", strconv.Itoa(i))
+			urlPhotoTV    = fmt.Sprintf("https://api.wallapop.com/api/v3/general/search?user_province=Madrid&latitude=40.41956&start=%s&user_region=Comunidad+de+Madrid&user_city=Madrid&search_id=1e5fb024-9140-495d-92b5-47710871c000&country_code=ES&user_postal_code=28014&experiment=freshness_factor_variation_D&items_count=200&density_type=20&filters_source=seo_landing&order_by=closest&step=0&category_ids=12545&longitude=-3.69196", strconv.Itoa(i))
 		)
 
 		// Сбор товаров из категории "Все для машин"
-		go func(urlSlug string, urlCarCat string) {
-			outCars = FindAll(urlSlug, urlCarCat, "Cars")
+		go func(urlSlug string, urlCar string) {
+			models.FindAllInCategory(urlCar, urlSlug, Cars)
 			defer wg.Done()
-		}(urlSlug, urlCarCat)
+		}(urlSlug, urlCar)
 
 		// Сбор товаров из категории "Электроника"
-		go func(urlSlug string, urlElectronicCat string) {
-			outElectronic = FindAll(urlSlug, urlElectronicCat, "Electronic")
+		go func(urlSlug string, urlElectronic string) {
+			models.FindAllInCategory(urlElectronic, urlSlug, Electronic)
 			defer wg.Done()
-		}(urlSlug, urlElectronicCat)
+		}(urlSlug, urlElectronic)
 
 		// Сбор товаров из категории "Все для дома"
-		go func(urlSlug string, urlHomeCat string) {
-			outHome = FindAll(urlSlug, urlHomeCat, "Home")
+		go func(urlSlug string, urlHome string) {
+			models.FindAllInCategory(urlHome, urlSlug, Home)
 			defer wg.Done()
-		}(urlSlug, urlHomeCat)
+		}(urlSlug, urlHome)
 
 		// Сбор товаров из категории "Мотоциклы"
-		go func(urlSlug string, urlMotosCat string) {
-			outMotos = FindAll(urlSlug, urlMotosCat, "Motos")
+		go func(urlSlug string, urlMotos string) {
+			models.FindAllInCategory(urlMotos, urlSlug, Motos)
 			defer wg.Done()
-		}(urlSlug, urlMotosCat)
+		}(urlSlug, urlMotos)
 
 		// Сбор товаров из категории "Все для Фото и Тв"
-		go func(urlSlug string, urlPhotoTVCat string) {
-			outPhotoTV = FindAll(urlSlug, urlPhotoTVCat, "PhotoTV")
+		go func(urlSlug string, urlPhotoTV string) {
+			models.FindAllInCategory(urlPhotoTV, urlSlug, PhotoTV)
 			defer wg.Done()
-		}(urlSlug, urlPhotoTVCat)
+		}(urlSlug, urlPhotoTV)
 
 		i = i + 40
 		wg.Wait()
@@ -62,57 +69,66 @@ func Run() {
 	/*
 		Отправка запроса в сервис хранилища
 	*/
-	fmt.Println("Машины и все что с ними связанно", string(outCars))
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-	fmt.Println("Электроника вся", string(outElectronic))
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-	fmt.Println("Все для дома", string(outHome))
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-	fmt.Println("Мотоциклы и все что с ними связанно", string(outMotos))
-	fmt.Printf("\n")
-	fmt.Printf("\n")
-	fmt.Println("Все для фото и тв", string(outPhotoTV))
+	fmt.Println("Машины и все что с ними связанно", string(MarshalData(models.Cars)))
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Электроника вся", string(MarshalData(models.Elec)))
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Все для дома", string(MarshalData(models.Home)))
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Мотоциклы и все что с ними связанно", string(MarshalData(models.Motos)))
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Все для фото и тв", string(MarshalData(models.PhotoTV)))
 }
 
-func FindAll(urlSlug string, url string, category string) []byte {
-	var out []byte
-	var err error
-
-	err = models.FindAllInCategory(url, urlSlug, category)
-
+func MarshalData(data interface{}) []byte {
+	out, err := gojson.Marshal(data)
 	if err != nil {
-		logrus.Infof("Err parce data - %s", err)
+		logrus.Errorf("Err marshal struct to json - %s", &err)
 	}
 
-	switch category {
-	case "Cars":
-		out, err = gojson.Marshal(models.CarsCat)
-		if err != nil {
-			logrus.Debugf("Err marshal struct to json Cars - %s", &err)
-		}
-	case "Electronic":
-		out, err = gojson.Marshal(models.ElecCat)
-		if err != nil {
-			logrus.Debugf("Err marshal struct to json Electronic - %s", &err)
-		}
-	case "Home":
-		out, err = gojson.Marshal(models.HomeCat)
-		if err != nil {
-			logrus.Debugf("Err marshal struct to json Home - %s", &err)
-		}
-	case "Motos":
-		out, err = gojson.Marshal(models.MotosCat)
-		if err != nil {
-			logrus.Debugf("Err marshal struct to json Motos - %s", &err)
-		}
-	case "PhotoTV":
-		out, err = gojson.Marshal(models.PhotoTVCat)
-		if err != nil {
-			logrus.Debugf("Err marshal struct to json PhoneTV - %s", &err)
-		}
-	}
 	return out
 }
+
+// func FindAll(urlSlug string, url string, category string) []byte {
+// 	var out []byte
+// 	var err error
+
+// 	err = models.FindAllInCategory(url, urlSlug, category)
+
+// 	if err != nil {
+// 		logrus.Infof("Err parce data - %s", err)
+// 	}
+
+// 	switch category {
+// 	case "Cars":
+// 		out, err = gojson.Marshal(models.CarsCat)
+// 		if err != nil {
+// 			logrus.Debugf("Err marshal struct to json Cars - %s", &err)
+// 		}
+// 	case "Electronic":
+// 		out, err = gojson.Marshal(models.ElecCat)
+// 		if err != nil {
+// 			logrus.Debugf("Err marshal struct to json Electronic - %s", &err)
+// 		}
+// 	case "Home":
+// 		out, err = gojson.Marshal(models.HomeCat)
+// 		if err != nil {
+// 			logrus.Debugf("Err marshal struct to json Home - %s", &err)
+// 		}
+// 	case "Motos":
+// 		out, err = gojson.Marshal(models.MotosCat)
+// 		if err != nil {
+// 			logrus.Debugf("Err marshal struct to json Motos - %s", &err)
+// 		}
+// 	case "PhotoTV":
+// 		out, err = gojson.Marshal(models.PhotoTVCat)
+// 		if err != nil {
+// 			logrus.Debugf("Err marshal struct to json PhoneTV - %s", &err)
+// 		}
+// 	}
+// 	return out
+// }
--- internal/models/wallapop.go
+++ internal/models/wallapop.go
@@ -84,284 +84,7 @@ type AllData struct {
 	} `json:"search_objects"`
 }
 
-// Структуры для формирования данных и дальнейшей их отправке (Из за паралельного подхода в сборе всех товаров со всех категорий пришлось прибегнуть к такому решения с множеством структур)
-type Cars struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Distance    float64 `json:"distance"`
-	Images      []struct {
-		Original       string `json:"original"`
-		Xsmall         string `json:"xsmall"`
-		Small          string `json:"small"`
-		Large          string `json:"large"`
-		Medium         string `json:"medium"`
-		Xlarge         string `json:"xlarge"`
-		OriginalWidth  int    `json:"original_width"`
-		OriginalHeight int    `json:"original_height"`
-	} `json:"images"`
-	User struct {
-		ID        string `json:"id"`
-		MicroName string `json:"micro_name"`
-		Image     struct {
-			Original       string `json:"original"`
-			Xsmall         string `json:"xsmall"`
-			Small          string `json:"small"`
-			Large          string `json:"large"`
-			Medium         string `json:"medium"`
-			Xlarge         string `json:"xlarge"`
-			OriginalWidth  int    `json:"original_width"`
-			OriginalHeight int    `json:"original_height"`
-		} `json:"image"`
-		Online bool   `json:"online"`
-		Kind   string `json:"kind"`
-	} `json:"user"`
-	Flags struct {
-		Pending  bool `json:"pending"`
-		Sold     bool `json:"sold"`
-		Reserved bool `json:"reserved"`
-		Banned   bool `json:"banned"`
-		Expired  bool `json:"expired"`
-		Onhold   bool `json:"onhold"`
-	} `json:"flags"`
-	VisibilityFlags struct {
-		Bumped        bool `json:"bumped"`
-		Highlighted   bool `json:"highlighted"`
-		Urgent        bool `json:"urgent"`
-		CountryBumped bool `json:"country_bumped"`
-		Boosted       bool `json:"boosted"`
-	} `json:"visibility_flags"`
-	Price        float64 `json:"price"`
-	Currency     string  `json:"currency"`
-	FreeShipping bool    `json:"free_shipping"`
-	WebSlug      string  `json:"web_slug"`
-	CategoryID   int     `json:"category_id"`
-	Shipping     struct {
-		ItemIsShippable     bool        `json:"item_is_shippable"`
-		UserAllowsShipping  bool        `json:"user_allows_shipping"`
-		CostConfigurationID interface{} `json:"cost_configuration_id"`
-	} `json:"shipping"`
-	SupportsShipping bool   `json:"supports_shipping"`
-	ShippingAllowed  bool   `json:"shipping_allowed"`
-	SellerID         string `json:"seller_id"`
-	Favorited        bool   `json:"favorited"`
-	CreationDate     int64  `json:"creation_date"`
-	ModificationDate int64  `json:"modification_date"`
-	Location         struct {
-		City        string `json:"city"`
-		PostalCode  string `json:"postal_code"`
-		CountryCode string `json:"country_code"`
-	} `json:"location"`
-}
-
-type Electronic struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Distance    float64 `json:"distance"`
-	Images      []struct {
-		Original       string `json:"original"`
-		Xsmall         string `json:"xsmall"`
-		Small          string `json:"small"`
-		Large          string `json:"large"`
-		Medium         string `json:"medium"`
-		Xlarge         string `json:"xlarge"`
-		OriginalWidth  int    `json:"original_width"`
-		OriginalHeight int    `json:"original_height"`
-	} `json:"images"`
-	User struct {
-		ID        string `json:"id"`
-		MicroName string `json:"micro_name"`
-		Image     struct {
-			Original       string `json:"original"`
-			Xsmall         string `json:"xsmall"`
-			Small          string `json:"small"`
-			Large          string `json:"large"`
-			Medium         string `json:"medium"`
-			Xlarge         string `json:"xlarge"`
-			OriginalWidth  int    `json:"original_width"`
-			OriginalHeight int    `json:"original_height"`
-		} `json:"image"`
-		Online bool   `json:"online"`
-		Kind   string `json:"kind"`
-	} `json:"user"`
-	Flags struct {
-		Pending  bool `json:"pending"`
-		Sold     bool `json:"sold"`
-		Reserved bool `json:"reserved"`
-		Banned   bool `json:"banned"`
-		Expired  bool `json:"expired"`
-		Onhold   bool `json:"onhold"`
-	} `json:"flags"`
-	VisibilityFlags struct {
-		Bumped        bool `json:"bumped"`
-		Highlighted   bool `json:"highlighted"`
-		Urgent        bool `json:"urgent"`
-		CountryBumped bool `json:"country_bumped"`
-		Boosted       bool `json:"boosted"`
-	} `json:"visibility_flags"`
-	Price        float64 `json:"price"`
-	Currency     string  `json:"currency"`
-	FreeShipping bool    `json:"free_shipping"`
-	WebSlug      string  `json:"web_slug"`
-	CategoryID   int     `json:"category_id"`
-	Shipping     struct {
-		ItemIsShippable     bool        `json:"item_is_shippable"`
-		UserAllowsShipping  bool        `json:"user_allows_shipping"`
-		CostConfigurationID interface{} `json:"cost_configuration_id"`
-	} `json:"shipping"`
-	SupportsShipping bool   `json:"supports_shipping"`
-	ShippingAllowed  bool   `json:"shipping_allowed"`
-	SellerID         string `json:"seller_id"`
-	Favorited        bool   `json:"favorited"`
-	CreationDate     int64  `json:"creation_date"`
-	ModificationDate int64  `json:"modification_date"`
-	Location         struct {
-		City        string `json:"city"`
-		PostalCode  string `json:"postal_code"`
-		CountryCode string `json:"country_code"`
-	} `json:"location"`
-}
-
-type Home struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Distance    float64 `json:"distance"`
-	Images      []struct {
-		Original       string `json:"original"`
-		Xsmall         string `json:"xsmall"`
-		Small          string `json:"small"`
-		Large          string `json:"large"`
-		Medium         string `json:"medium"`
-		Xlarge         string `json:"xlarge"`
-		OriginalWidth  int    `json:"original_width"`
-		OriginalHeight int    `json:"original_height"`
-	} `json:"images"`
-	User struct {
-		ID        string `json:"id"`
-		MicroName string `json:"micro_name"`
-		Image     struct {
-			Original       string `json:"original"`
-			Xsmall         string `json:"xsmall"`
-			Small          string `json:"small"`
-			Large          string `json:"large"`
-			Medium         string `json:"medium"`
-			Xlarge         string `json:"xlarge"`
-			OriginalWidth  int    `json:"original_width"`
-			OriginalHeight int    `json:"original_height"`
-		} `json:"image"`
-		Online bool   `json:"online"`
-		Kind   string `json:"kind"`
-	} `json:"user"`
-	Flags struct {
-		Pending  bool `json:"pending"`
-		Sold     bool `json:"sold"`
-		Reserved bool `json:"reserved"`
-		Banned   bool `json:"banned"`
-		Expired  bool `json:"expired"`
-		Onhold   bool `json:"onhold"`
-	} `json:"flags"`
-	VisibilityFlags struct {
-		Bumped        bool `json:"bumped"`
-		Highlighted   bool `json:"highlighted"`
-		Urgent        bool `json:"urgent"`
-		CountryBumped bool `json:"country_bumped"`
-		Boosted       bool `json:"boosted"`
-	} `json:"visibility_flags"`
-	Price        float64 `json:"price"`
-	Currency     string  `json:"currency"`
-	FreeShipping bool    `json:"free_shipping"`
-	WebSlug      string  `json:"web_slug"`
-	CategoryID   int     `json:"category_id"`
-	Shipping     struct {
-		ItemIsShippable     bool        `json:"item_is_shippable"`
-		UserAllowsShipping  bool        `json:"user_allows_shipping"`
-		CostConfigurationID interface{} `json:"cost_configuration_id"`
-	} `json:"shipping"`
-	SupportsShipping bool   `json:"supports_shipping"`
-	ShippingAllowed  bool   `json:"shipping_allowed"`
-	SellerID         string `json:"seller_id"`
-	Favorited        bool   `json:"favorited"`
-	CreationDate     int64  `json:"creation_date"`
-	ModificationDate int64  `json:"modification_date"`
-	Location         struct {
-		City        string `json:"city"`
-		PostalCode  string `json:"postal_code"`
-		CountryCode string `json:"country_code"`
-	} `json:"location"`
-}
-
-type Motos struct {
-	ID          string  `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Distance    float64 `json:"distance"`
-	Images      []struct {
-		Original       string `json:"original"`
-		Xsmall         string `json:"xsmall"`
-		Small          string `json:"small"`
-		Large          string `json:"large"`
-		Medium         string `json:"medium"`
-		Xlarge         string `json:"xlarge"`
-		OriginalWidth  int    `json:"original_width"`
-		OriginalHeight int    `json:"original_height"`
-	} `json:"images"`
-	User struct {
-		ID        string `json:"id"`
-		MicroName string `json:"micro_name"`
-		Image     struct {
-			Original       string `json:"original"`
-			Xsmall         string `json:"xsmall"`
-			Small          string `json:"small"`
-			Large          string `json:"large"`
-			Medium         string `json:"medium"`
-			Xlarge         string `json:"xlarge"`
-			OriginalWidth  int    `json:"original_width"`
-			OriginalHeight int    `json:"original_height"`
-		} `json:"image"`
-		Online bool   `json:"online"`
-		Kind   string `json:"kind"`
-	} `json:"user"`
-	Flags struct {
-		Pending  bool `json:"pending"`
-		Sold     bool `json:"sold"`
-		Reserved bool `json:"reserved"`
-		Banned   bool `json:"banned"`
-		Expired  bool `json:"expired"`
-		Onhold   bool `json:"onhold"`
-	} `json:"flags"`
-	VisibilityFlags struct {
-		Bumped        bool `json:"bumped"`
-		Highlighted   bool `json:"highlighted"`
-		Urgent        bool `json:"urgent"`
-		CountryBumped bool `json:"country_bumped"`
-		Boosted       bool `json:"boosted"`
-	} `json:"visibility_flags"`
-	Price        float64 `json:"price"`
-	Currency     string  `json:"currency"`
-	FreeShipping bool    `json:"free_shipping"`
-	WebSlug      string  `json:"web_slug"`
-	CategoryID   int     `json:"category_id"`
-	Shipping     struct {
-		ItemIsShippable     bool        `json:"item_is_shippable"`
-		UserAllowsShipping  bool        `json:"user_allows_shipping"`
-		CostConfigurationID interface{} `json:"cost_configuration_id"`
-	} `json:"shipping"`
-	SupportsShipping bool   `json:"supports_shipping"`
-	ShippingAllowed  bool   `json:"shipping_allowed"`
-	SellerID         string `json:"seller_id"`
-	Favorited        bool   `json:"favorited"`
-	CreationDate     int64  `json:"creation_date"`
-	ModificationDate int64  `json:"modification_date"`
-	Location         struct {
-		City        string `json:"city"`
-		PostalCode  string `json:"postal_code"`
-		CountryCode string `json:"country_code"`
-	} `json:"location"`
-}
-
-type PhotoTV struct {
+type RequestLast struct {
 	ID          string  `json:"id"`
 	Title       string  `json:"title"`
 	Description string  `json:"description"`
@@ -432,21 +155,20 @@ type PhotoTV struct {
 
 // Объявление структур
 var (
-	CarsCat    []Cars
-	ElecCat    []Electronic
-	HomeCat    []Home
-	MotosCat   []Motos
-	PhotoTVCat []PhotoTV
+	Cars    []RequestLast
+	Elec    []RequestLast
+	Home    []RequestLast
+	Motos   []RequestLast
+	PhotoTV []RequestLast
 )
 
 // Основная универсальная функция для работы со всеми категориями
-func FindAllInCategory(url string, urlSlug string, category string) error {
+func FindAllInCategory(url string, urlSlug string, category string) {
 	var all AllData
 
 	resp, err := http.Get(url)
 	if err != nil {
 		logrus.Warnf("Error request to wallapop data - %s", err)
-		return err
 	}
 	defer resp.Body.Close()
 
@@ -458,137 +180,50 @@ func FindAllInCategory(url string, urlSlug string, category string) error {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		logrus.Warnf("Err parce resp body - %s", err)
-		return err
 	}
 
 	if err := gojson.Unmarshal(body, &all); err != nil {
 		logrus.Errorf("Err unmarshal json to struct - %s", err)
-		return err
 	}
 
-	for _, val := range all.SearchObjects {
-		switch category {
-		case "Cars":
-			CarsCat = append(CarsCat, Cars{
-				ID:               val.ID,
-				Title:            val.Title,
-				Description:      val.Description,
-				Distance:         val.Distance,
-				Images:           val.Images,
-				User:             val.User,
-				Flags:            val.Flags,
-				VisibilityFlags:  val.VisibilityFlags,
-				Price:            val.Price,
-				Currency:         val.Currency,
-				FreeShipping:     val.FreeShipping,
-				WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
-				CategoryID:       val.CategoryID,
-				Shipping:         val.Shipping,
-				SupportsShipping: val.SupportsShipping,
-				ShippingAllowed:  val.ShippingAllowed,
-				SellerID:         val.SellerID,
-				Favorited:        val.Favorited,
-				CreationDate:     val.CreationDate,
-				ModificationDate: val.ModificationDate,
-				Location:         val.Location,
-			})
-		case "Electronic":
-			ElecCat = append(ElecCat, Electronic{
-				ID:               val.ID,
-				Title:            val.Title,
-				Description:      val.Description,
-				Distance:         val.Distance,
-				Images:           val.Images,
-				User:             val.User,
-				Flags:            val.Flags,
-				VisibilityFlags:  val.VisibilityFlags,
-				Price:            val.Price,
-				Currency:         val.Currency,
-				FreeShipping:     val.FreeShipping,
-				WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
-				CategoryID:       val.CategoryID,
-				Shipping:         val.Shipping,
-				SupportsShipping: val.SupportsShipping,
-				ShippingAllowed:  val.ShippingAllowed,
-				SellerID:         val.SellerID,
-				Favorited:        val.Favorited,
-				CreationDate:     val.CreationDate,
-				ModificationDate: val.ModificationDate,
-				Location:         val.Location,
-			})
-		case "Home":
-			HomeCat = append(HomeCat, Home{
-				ID:               val.ID,
-				Title:            val.Title,
-				Description:      val.Description,
-				Distance:         val.Distance,
-				Images:           val.Images,
-				User:             val.User,
-				Flags:            val.Flags,
-				VisibilityFlags:  val.VisibilityFlags,
-				Price:            val.Price,
-				Currency:         val.Currency,
-				FreeShipping:     val.FreeShipping,
-				WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
-				CategoryID:       val.CategoryID,
-				Shipping:         val.Shipping,
-				SupportsShipping: val.SupportsShipping,
-				ShippingAllowed:  val.ShippingAllowed,
-				SellerID:         val.SellerID,
-				Favorited:        val.Favorited,
-				CreationDate:     val.CreationDate,
-				ModificationDate: val.ModificationDate,
-				Location:         val.Location,
-			})
-		case "Motos":
-			MotosCat = append(MotosCat, Motos{
-				ID:               val.ID,
-				Title:            val.Title,
-				Description:      val.Description,
-				Distance:         val.Distance,
-				Images:           val.Images,
-				User:             val.User,
-				Flags:            val.Flags,
-				VisibilityFlags:  val.VisibilityFlags,
-				Price:            val.Price,
-				Currency:         val.Currency,
-				FreeShipping:     val.FreeShipping,
-				WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
-				CategoryID:       val.CategoryID,
-				Shipping:         val.Shipping,
-				SupportsShipping: val.SupportsShipping,
-				ShippingAllowed:  val.ShippingAllowed,
-				SellerID:         val.SellerID,
-				Favorited:        val.Favorited,
-				CreationDate:     val.CreationDate,
-				ModificationDate: val.ModificationDate,
-				Location:         val.Location,
-			})
-		case "PhotoTV":
-			PhotoTVCat = append(PhotoTVCat, PhotoTV{
-				ID:               val.ID,
-				Title:            val.Title,
-				Description:      val.Description,
-				Distance:         val.Distance,
-				Images:           val.Images,
-				User:             val.User,
-				Flags:            val.Flags,
-				VisibilityFlags:  val.VisibilityFlags,
-				Price:            val.Price,
-				Currency:         val.Currency,
-				FreeShipping:     val.FreeShipping,
-				WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
-				CategoryID:       val.CategoryID,
-				Shipping:         val.Shipping,
-				SupportsShipping: val.SupportsShipping,
-				ShippingAllowed:  val.ShippingAllowed,
-				SellerID:         val.SellerID,
-				Favorited:        val.Favorited,
-				CreationDate:     val.CreationDate,
-				ModificationDate: val.ModificationDate,
-				Location:         val.Location,
-			})
-		}
+	switch category {
+	case "Cars":
+		AppendData(&all, &Cars, urlSlug)
+	case "Electronic":
+		AppendData(&all, &Elec, urlSlug)
+	case "Home":
+		AppendData(&all, &Home, urlSlug)
+	case "Motos":
+		AppendData(&all, &Motos, urlSlug)
+	case "PhotoTV":
+		AppendData(&all, &PhotoTV, urlSlug)
 	}
-	return nil
+}
+
+func AppendData(all *AllData, data *[]RequestLast, urlSlug string) {
+	for _, val := range all.SearchObjects {
+		*data = append(*data, RequestLast{
+			ID:               val.ID,
+			Title:            val.Title,
+			Description:      val.Description,
+			Distance:         val.Distance,
+			Images:           val.Images,
+			User:             val.User,
+			Flags:            val.Flags,
+			VisibilityFlags:  val.VisibilityFlags,
+			Price:            val.Price,
+			Currency:         val.Currency,
+			FreeShipping:     val.FreeShipping,
+			WebSlug:          fmt.Sprintf("%s/%s", urlSlug, val.WebSlug),
+			CategoryID:       val.CategoryID,
+			Shipping:         val.Shipping,
+			SupportsShipping: val.SupportsShipping,
+			ShippingAllowed:  val.ShippingAllowed,
+			SellerID:         val.SellerID,
+			Favorited:        val.Favorited,
+			CreationDate:     val.CreationDate,
+			ModificationDate: val.ModificationDate,
+			Location:         val.Location,
+		})
+	}
 }
